1b7a6bfa1f16098de482cc86e0bdd0fe5c24279c
Douglas Liao <douglasliaozy@gmail.com>
2022-06-18 17:11:55 -0300

    Adicionando formulario

--- ProjetoEspecializado/settings.py
+++ ProjetoEspecializado/settings.py
@@ -130,3 +130,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email teste console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+"""
+# Email produção
+
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = 'no-reply@empresa.com.br'
+EMAIL_PORT = 587
+EMAIL_USE_TSL = True
+EMAIL_HOST_PASSWORD = 'user'
+DEFAULT_FROM_EMAIL = 'contato@empresa.com.br'
+"""
